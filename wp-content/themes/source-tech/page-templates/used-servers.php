39e142163607f57bf48971fd8c74b7162e55072f
Chris Hubbard <chris@markethubb.com>
2020-11-15 17:56:57 -0500

    Update: Retrieve content for Used Servers page from new shared template

--- wp-content/themes/source-tech/page-templates/used-servers.php
+++ wp-content/themes/source-tech/page-templates/used-servers.php
@@ -17,64 +17,8 @@ get_header(); ?>
         </div>
     </div>
 </div>
-    
-    <?php 
-    if( have_rows('page_refurbished_servers_manufacturers') ):
-        $servers = '';
-        while ( have_rows('page_refurbished_servers_manufacturers') ) : the_row();
-            $manufacturer = get_sub_field('page_refurbished_servers_manufacturers_name');
-            $product_line = get_sub_field('page_refurbished_servers_product_line');
-            $accent_color = get_sub_field('page_refurbished_servers_color');
-            $logo = get_sub_field('page_refurbished_servers_manufacturers_logo');
-            $manufacturer_description = get_sub_field('page_refurbished_servers_manufacturers_description');
 
-            $servers .= '<div class="manufacturer-container">';
-
-//            $servers .= '<div class="container d-flex h-100 mb-4">';
-//            $servers .= '<div class="row align-items-center h-100">';
-//            $servers .= '<div class="col-md-3 mx-auto">';
-//            $servers .= '<img src="' . get_sub_field('page_refurbished_servers_manufacturers_logo') . '" class="manufacturer-logo" />';
-//            $servers .= '</div>';
-//            $servers .= '<div class="col-md-9 mx-auto">';
-//            $servers .= '<p class="manufacturer-desc mb-0">' . get_sub_field('page_refurbished_servers_manufacturers_description') . '</p>';
-//            $servers .= '</div></div></div>';
-
-            if( have_rows('page_refurbished_servers_form_factors') ):
-
-                $servers .= '<div class="container-fluid fluid-constrained d-flex h-100">';
-                $servers .= '<div class="row row-eq-height">';
-
-                $servers .= '<div class="col-md-3 mx-auto">';
-                $servers .= '<div class="v-align-center h-100">';
-                $servers .= '<img src="' . $logo . '" class="manufacturer-logo" />';
-                $servers .= '<p class="manufacturer-desc mb-0">' . $manufacturer_description . '</p>';
-                $servers .= '</div></div>';
-
-                while ( have_rows('page_refurbished_servers_form_factors') ) : the_row();
-                    $form_factor = get_sub_field('page_refurbished_servers_form_factors_type');
-                    $tax = get_sub_field('page_refurbished_servers_form_factors_link');
-
-                    $servers .= '<div class="col-md-3 text-center">';
-                    $servers .= '<div class="panel shadow h-100 bg-white" ';
-                    $servers .= 'style="border-top: 8px solid ' . $accent_color . '">';
-                    $servers .= '<p class="form-manufacturer">' . $product_line . '</p>';
-                    $servers .= '<h3 class="form-factor pb-2">' . get_sub_field('page_refurbished_servers_form_factors_type') . '</h3>';
-                    $servers .= '<img src="' . get_sub_field('page_refurbished_servers_form_factors_image') . '" class="manufacturer-logo mt-3 mb-4" />';
-                    $servers .= '<a href="' . get_term_link($tax[0]) . '" class="form-factor-links">';
-                    $servers .= 'Shop ' . $manufacturer . ' ' . $form_factor . ' <i class="far fa-arrow-right"></i></a>';
-                    $servers .= '<p class="product-count"><strong>' . get_product_post_count(get_sub_field('page_refurbished_servers_form_factors_link')) . '</strong>';
-                    $servers .= ' models in stock</p>';
-                    $servers .= '</div></div>';
-
-                endwhile;
-                $servers .= '</div></div>';
-            endif;
-            $servers .= '</div>';
-
-        endwhile;
-        echo $servers;
-    endif;
-    ?>
+<?php get_template_part('template-parts/servers/content', 'manufacturers'); ?>
         
 </div>
 
