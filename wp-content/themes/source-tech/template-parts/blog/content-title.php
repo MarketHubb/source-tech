2d779a569096e2aedce64c2fc9cbf735a5bdaa7f
Chris Hubbard <chris@markethubb.com>
2020-11-30 12:29:05 -0500

    Update: Enhance single-post page template layout

--- wp-content/themes/source-tech/template-parts/blog/content-title.php
+++ wp-content/themes/source-tech/template-parts/blog/content-title.php
@@ -5,10 +5,10 @@ $sub_heading = (get_field('post_banner_subtitle')) ? get_field('post_banner_subt
 $dates = compare_published_updated_dates($post->ID);
 $published_read = '';
 foreach ($dates as $key => $val) {
-    $published_read .= '<p><i class="fas fa-calendar-week"></i> ' . ucwords($key) . ': <strong>' . $val . '</strong></p>';
+    $published_read .= '<p><i class="fas fa-calendar-week"></i> ' . ucwords($key) . ': ' . $val . '</p>';
 }
 if (get_field('post_read_time')) {
-    $published_read .= '<p><i class="fas fa-clock"></i> Read Time: ' . '<strong>' . get_field('post_read_time') . ' minutes</strong></p>';
+    $published_read .= '<p><i class="fas fa-clock"></i> Read Time: ' . get_field('post_read_time') . ' minutes</p>';
 }
 ?>
 
@@ -31,12 +31,12 @@ if (get_field('post_read_time')) {
     $banner_url = get_field('post_featured_image');
 ?>
 <!-- Page Title (Banner) -->
-<div class="container-fluid p-0">
+<div class="container-fluid p-0" id="single-post-banner">
     <div class="banner-container" style="background-image: url('<?php echo $banner_url; ?>');">
         <div class="banner-overlay dark">
             <div class="container h-100 py-5">
-                <div class="row h-100 pt-4 pb-4 align-items-center">
-                    <div class="col-12 text-center">
+                <div class="row h-100 pt-4 pb-5 align-items-center justify-content-center">
+                    <div class="col-md-10 text-center">
                         <?php
                         $cats = '<ul class="post-categories">';
                         foreach ($categories as $category) {
@@ -47,7 +47,7 @@ if (get_field('post_read_time')) {
                         $cats .= '</ul>';
                         echo $cats;
                         ?>
-                        <h1 class="banner-headline mt-2"><?php echo $heading; ?></h1>
+                        <h1 class="banner-headline mt-2 mb-2"><?php echo $heading; ?></h1>
                         <p class="banner-subheadline mb-5"><?php echo $sub_heading; ?></p>
                         <div class="publish-container">
                             <?php echo $published_read; ?>
--- wp-content/themes/source-tech/template-parts/blog/content.php
+++ wp-content/themes/source-tech/template-parts/blog/content.php
@@ -1,69 +1,71 @@
-<?php
-$categories = get_the_category();
-?>
-<div class="container post-content-container">
-    <div class="row bg-white pt-4">
-        <div class="col">
-            <?php
-            if (!get_field('post_content_migrated')) {
-                the_content();
-            } else {
-                if( have_rows('post_section') ):
-                    $content = '';
-                    if( have_rows('post_overview') ):
-                        $content .= '<div class="row post-overview">';
-                        $content .= '<div class="col">';
-                        $content .= '<ul>';
-                        while ( have_rows('post_overview') ) : the_row();
-                            $content .= '<li>' .get_sub_field('post_overview_bullet') . '</li>';
-                        endwhile;
-                        $content .= '</ul></div></div>';
-                    endif;
+<?php $categories = get_the_category(); ?>
+
+<div class="container-fluid" id="single-post">
+    <div class="wrapper">
+        <div class="row">
+            <div class="col shadow">
+
+                <?php
+                if (!get_field('post_content_migrated')) {
+                    the_content();
+                } else {
+                    if( have_rows('post_section') ):
+                        $content = '';
+                        if( have_rows('post_overview') ):
+                            $content .= '<div class="post-content-section px-5" id="post-overview">';
+                            $content .= '<ul>';
+                            while ( have_rows('post_overview') ) : the_row();
+                                $content .= '<li>' .get_sub_field('post_overview_bullet') . '</li>';
+                            endwhile;
+                            $content .= '</ul></div>';
+                        endif;
 
 
-                    $content .= '<div class="row">';
-                    while ( have_rows('post_section') ) : the_row();
-                        $content .= '<div class="post-content-section">';
-                        $section_heading = get_sub_field('post_section_content_heading');
+//                        $content .= '<div class="row">';
+                        while ( have_rows('post_section') ) : the_row();
+                            $content .= '<div class="post-content-section px-5">';
+                            $section_heading = get_sub_field('post_section_content_heading');
 
-                        if ($section_heading) {
-                            $content .= '<h3>' . $section_heading . '</h3>';
-                        }
+                            if ($section_heading) {
+                                $content .= '<h3>' . $section_heading . '</h3>';
+                            }
 
 
-                        if (get_sub_field('post_section_content_type') == 'Text') {
-                            $content .= get_sub_field('post_section_content_body');
-                        }
-                        if (get_sub_field('post_section_content_type') == 'Callout') {
-                            $content .= '<div class="post-callout bg-blue-grey">';
-                            $content .= '<p>' . get_sub_field('post_section_callout') . '</p>';
+                            if (get_sub_field('post_section_content_type') == 'Text') {
+                                $content .= get_sub_field('post_section_content_body');
+                            }
+                            if (get_sub_field('post_section_content_type') == 'Callout') {
+                                $content .= '<div class="post-callout bg-blue-grey">';
+                                $content .= '<p>' . get_sub_field('post_section_callout') . '</p>';
+                                $content .= '</div>';
+                            }
+                            if (get_sub_field('post_section_content_type') == 'Images') {
+                                $row_count = 12 / count(get_sub_field('post_section_images'));
+
+                                if( have_rows('post_section_images') ):
+                                    $content .= '<div class="row post-images">';
+                                    while ( have_rows('post_section_images') ) : the_row();
+                                        $content .= '<div class="col-md-' . $row_count . '">';
+                                        $content .= '<div class="text-center">';
+                                        $content .= '<img src="' . get_sub_field('post_section_images_image') . '" />';
+                                        $content .= '</div>';
+                                        $content .= '<p>' . get_sub_field('post_section_images_description') . '</p>';
+                                        $content .= '</div>';
+                                    endwhile;
+                                    $content .= '</div>';
+
+                                endif;
+
+                            }
                             $content .= '</div>';
-                        }
-                        if (get_sub_field('post_section_content_type') == 'Images') {
-                            $row_count = 12 / count(get_sub_field('post_section_images'));
+                        endwhile;
+//                        $content .= '</div>';
+                    endif;
+                    echo $content;
+                }
+                ?>
 
-                            if( have_rows('post_section_images') ):
-                                $content .= '<div class="row post-images">';
-                                while ( have_rows('post_section_images') ) : the_row();
-                                    $content .= '<div class="col-md-' . $row_count . '">';
-                                    $content .= '<div class="text-center">';
-                                    $content .= '<img src="' . get_sub_field('post_section_images_image') . '" />';
-                                    $content .= '</div>';
-                                    $content .= '<p>' . get_sub_field('post_section_images_description') . '</p>';
-                                    $content .= '</div>';
-                                endwhile;
-                                    $content .= '</div>';
-
-                            endif;
-
-                        }
-                        $content .= '</div>';
-                    endwhile;
-                    $content .= '</div>';
-                endif;
-                echo $content;
-            }
-            ?>
+            </div>
         </div>
     </div>
 </div>
